2eeb1829483d66fde3453035917a6e5649de5f3b
Daniel Burgess <37884090+Danb981@users.noreply.github.com>
2023-11-23 19:40:41 -0500

    Removed unnecessary code

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
 
 var app = express();
@@ -14,11 +11,8 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 module.exports = app;
--- controllers/SocketController.js
+++ controllers/SocketController.js
@@ -7,11 +7,9 @@ const io = require("socket.io")({
   }
 });
 
-let gamePrompts = ["Test"];
-/*let gamePrompts = ["You do not have to sit outside in the dark. If, however, you want to look at the stars, you will find that darkness is necessary. But the stars neither require nor demand it.",
-"For all its apparent strength, the Aztec Empire had vulnerabilities that the Spanish were able to exploit. The tiny Spanish force was able to attract a massive army of some 30,000 Mesoamericans of many different tribes.",
-"Imagine this: Here we are, a plane full of grown human beings, many of us partially educated, and they're actually taking time out to describe the intricate workings of a belt buckle. Well, I ask for clarification at that point. Did you say 'Place the small metal flap into the buckle' or 'Place the buckle over and around the small metal flap'?"];
-*/
+let gamePrompts = ["You do not have to sit outside in the dark. If, however, you want to look at the stars, you will find that darkness is necessary. But the stars neither require nor demand it.",
+  "Imagine this: Here we are, a plane full of grown human beings, many of us partially educated, and they're actually taking time out to describe the intricate workings of a belt buckle. Well, I ask for clarification at that point. Did you say 'Place the small metal flap into the buckle' or 'Place the buckle over and around the small metal flap'?"];
+
 const getRandomPrompt = () => {
   return gamePrompts[Math.floor(Math.random() * gamePrompts.length)];
 }
@@ -25,34 +23,36 @@ io.on("connection", (socket) => {
   queue.push(socket);
   socket.on("disconnect", () => {
     console.log("Connection destroyed id: " + socket.id);
-    for(let i = 0; i < queue.length; i++){
-      if(queue[i].id == socket.id){
+    for (let i = 0; i < queue.length; i++) {
+      if (queue[i].id == socket.id) {
         queue.splice(i, 1);
       }
     }
   });
+
   socket.on("report_player_progress", (data) => {
     let game = activeGames[data.gameId];
     game.participants[socket.id].progress = data.playerProgress;
     game.participants[socket.id].wordsTyped = data.wordsTyped;
     for (const id in activeGames[data.gameId].participants) {
-      if (!(id == socket.id)) { //dont send update to user who reported progress
+      if (!id != socket.id) { //dont send update to user who reported progress
         io.to(id).emit('report_participant_progress', activeGames[data.gameId].participants);
       }
     }
   });
+
   socket.on("player_finished", (data) => {
     let game = activeGames[data.gameId];
     game.podium.push(socket.id);
     io.to(socket.id).emit('report_player_result', game.podium.length);
-    if(game.podium.length == Object.keys(game.participants).length){ //game concluded
-      delete(activeGames[data.gameId]);
+    if (game.podium.length == Object.keys(game.participants).length) { //game concluded
+      delete (activeGames[data.gameId]);
     }
   });
 });
 
-const scheduleGameInterval = setInterval(() => {
-  if(queue.length >= 2){
+const scheduleGameInterval = setInterval(() => { //interval to create match from queued players
+  while (queue.length >= 2) {
     scheduleGame();
   }
 }, 10000);
@@ -61,13 +61,13 @@ const scheduleGame = () => {
   let participants = {};
   let participantsCount = Math.max(queue.length, 4);
   queue.splice(0, participantsCount).forEach((participant) => {
-    participants[participant.id] = {progress: 0, wordsTyped: 0};
+    participants[participant.id] = { progress: 0, wordsTyped: 0 };
   });
   let gameId = uuidv4();
   let gameStartTime = Date.now() + 5000;
   let prompt = getRandomPrompt();
   activeGames[gameId] = { prompt, participants, gameStartTime, podium: [] };
   for (const id in activeGames[gameId].participants) {
-    io.to(id).emit('schedule_online_game', { gameId, id, prompt, gameStartTime, participants: activeGames[gameId].participants});
+    io.to(id).emit('schedule_online_game', { gameId, id, prompt, gameStartTime, participants: activeGames[gameId].participants });
   }
 }
